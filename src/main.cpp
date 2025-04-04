67d5a20119011b15a21fccf8586e3ba8a593fedd
Drew <145530440+DrewRoss5@users.noreply.github.com>
2025-04-04 15:08:17 -0600

    Display pushing and popping times seprately
    Additionally, this disables output by default

--- src/main.cpp
+++ src/main.cpp
@@ -24,30 +24,41 @@ int main(int argc, char** argv){
     ArrayStack<int> arr_stack;
     for (int i = 0; i < num_elems; i++)
         arr_stack.push(i);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto push_time = std::chrono::duration<double, std::milli>(t2 - t1);
+    std::cout << "Time to push " << num_elems << " elements: " << push_time.count() << "ms" << std::endl;
+    t1 = std::chrono::high_resolution_clock::now();
     while (!arr_stack.empty()){
-        printf("%d\n", arr_stack.top()); // printf is used to minimize run times
+        //printf("%d\n", arr_stack.top()); // printf is used to minimize run times
         arr_stack.pop();
     }
-    auto t2 = std::chrono::high_resolution_clock::now();
-    // get the run time for the array-based stack
-    auto duration = std::chrono::duration<double, std::milli>(t2 - t1);
-    double arr_time = duration.count();
+    t2 = std::chrono::high_resolution_clock::now();
+    auto pop_time = std::chrono::duration<double, std::milli>(t2 - t1);
+    std::cout << "Time to pop " << num_elems << " elements: " << pop_time.count() << "ms" << std::endl;
+    auto arr_time = push_time + pop_time;
     // benchmark the list based stack
     std::cout << "-------------------------------\nBenchmarking List-Based Stack:\n-------------------------------" << std::endl;
     t1 = std::chrono::high_resolution_clock::now();
     LinkedStack<int> linked_stack;
     for (int i = 0; i < num_elems; i++)
         linked_stack.push(i);
+    t2 = std::chrono::high_resolution_clock::now();
+    push_time = std::chrono::duration<double, std::milli>(t2 - t1);
+    std::cout << "Time to push " << num_elems << " elements: " << push_time.count() << "ms" << std::endl;
+    t1 = std::chrono::high_resolution_clock::now();
     while (!linked_stack.empty()){
-        printf("%d\n", linked_stack.top()); // printf is used to minimize run times
+        //printf("%d\n", linked_stack.top()); // printf is used to minimize run times
         linked_stack.pop();
     }
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration<double, std::milli>(t2 - t1);
-    double list_time = duration.count();
+    pop_time = std::chrono::duration<double, std::milli>(t2 - t1);
+    std::cout << "Time to pop " << num_elems << " elements: " << pop_time.count() << "ms" << std::endl;
+    auto list_time = pop_time + push_time;
     // display the results of the benchmark
-    std::cout << "Array-based time: " << arr_time << "ms\n" << "List-based time: " << list_time << "ms" << std::endl;
+    std::cout << "-------------------------------\nResults:\n------------------------------\n";
+    std::cout << "Array-based time: " << arr_time.count() << "ms\n" << "List-based time: " << list_time.count() << "ms" << std::endl;
     // determine which implementation is faster and display it to the user
+    
     if (arr_time < list_time){
         double margin = ((list_time - arr_time) / list_time) * 100;
         std::cout << "The array-based stack was  " << margin << "% faster than the list-based stack." << std::endl;
